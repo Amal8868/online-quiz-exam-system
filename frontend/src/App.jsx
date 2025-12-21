5481a0ab956c1d8e387976710fad5df4a8d7ee0c
Amal Abdulkadir <Amalabdulkadir67@gmail.com>
2025-12-21 15:20:07 +0300

    Add sample CSV roster

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,15 @@ import TeacherLogin from './pages/auth/TeacherLogin';
 import TeacherRegister from './pages/auth/TeacherRegister';
 import TeacherDashboard from './pages/teacher/Dashboard';
 import CreateQuiz from './pages/teacher/CreateQuiz';
+import QuizManage from './pages/teacher/QuizManage';
+import QuizResults from './pages/teacher/QuizResults'; // New Import
 import StudentJoin from './pages/student/Join';
+import WaitingRoom from './pages/student/WaitingRoom';
 import ExamPage from './pages/student/ExamPage';
 import ResultPage from './pages/student/ResultPage';
+import ClassList from './pages/teacher/ClassList';
+import ClassDetails from './pages/teacher/ClassDetails';
+import GlobalImport from './pages/teacher/GlobalImport';
 import NotFound from './pages/NotFound';
 
 function App() {
@@ -19,23 +25,29 @@ function App() {
         <Routes>
           {/* Public Routes */}
           <Route path="/" element={<LandingPage />} />
-          
+
           {/* Auth Routes */}
           <Route path="/teacher/login" element={<TeacherLogin />} />
           <Route path="/teacher/register" element={<TeacherRegister />} />
-          
+
           {/* Teacher Routes */}
           <Route path="/teacher" element={<Layout type="teacher" />}>
             <Route index element={<TeacherDashboard />} />
             <Route path="create-quiz" element={<CreateQuiz />} />
+            <Route path="quiz/:quizId" element={<QuizManage />} />
+            <Route path="quiz/:quizId/results" element={<QuizResults />} /> {/* New Route */}
+            <Route path="classes" element={<ClassList />} />
+            <Route path="classes/:id" element={<ClassDetails />} />
+            <Route path="classes/import" element={<GlobalImport />} />
             {/* Add more teacher routes here */}
           </Route>
-          
+
           {/* Student Routes */}
           <Route path="/join" element={<StudentJoin />} />
+          <Route path="/student/waiting/:quizId" element={<WaitingRoom />} />
           <Route path="/exam/:roomCode" element={<ExamPage />} />
           <Route path="/results/:resultId" element={<ResultPage />} />
-          
+
           {/* 404 Route */}
           <Route path="*" element={<NotFound />} />
         </Routes>
